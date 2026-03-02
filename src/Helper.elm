f0ffd1a31faa306ef268c1d3878f2f3eea93ab37
up230343-beep <up230343@alumnos.upa.edu.mx>
2026-03-01 21:31:30 -0600

    Tarea UP230343

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,52 @@
 module Helper exposing (..)
+
+
+type Grade
+    = Approved
+    | Failed
+    | Pending
+
+
+type AirplaneStatus
+    = Cancelled
+    | Delayed
+    | OnTime
+    | Boarding
+
+
+categoricalGrade : List Float -> List Grade
+categoricalGrade grades =
+    List.map gradeFromFloat grades
+
+
+gradeFromFloat : Float -> Grade
+gradeFromFloat grade =
+    if grade >= 7 then
+        Approved
+
+    else if grade >= 0 then
+        Failed
+
+    else
+        Pending
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
